61d9c78264306336b98df4d93f8d59e109e1e565
Aaron <the_flicker7@hotmail.com>
2020-10-29 16:50:53 -0400

    adding router, manage courses, and add courses

---
+++ ps-flux/src/components/ManageCoursePage.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+
+const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value,
+    });
+  }
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+      <CourseForm course={course} onChange={handleChange} />
+    </>
+  );
+};
+
+export default ManageCoursePage;
